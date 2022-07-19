ec325852df4d7c90edf1429f0da76ebb10f23e9c
hamzaabdous <abdoushamza6@gmail.com>
2022-07-19 21:46:15 +0100

    api role [ update / delete ]

--- app/Modules/Role/Http/Controllers/RoleController.php
+++ app/Modules/Role/Http/Controllers/RoleController.php
@@ -57,4 +57,54 @@ class RoleController extends Controller
             "status" => "200"
         ];
     }
+
+    public function update(Request $request){
+        $validator = Validator::make($request->all(), [
+            "id" => "required",
+        ]);
+        if ($validator->fails()) {
+            return [
+                "payload" => $validator->errors(),
+                "status" => "406_2"
+            ];
+        }
+        $role=Role::find($request->id);
+        if (!$role) {
+            return [
+                "payload" => "The searched row does not exist !",
+                "status" => "404_3"
+            ];
+        }
+        if($request->name!=$role->name){
+            if(Role::where("name",$request->name)->count()>0)
+                return [
+                    "payload" => "The Role has been already taken ! ",
+                    "status" => "406_2"
+                ];
+        }
+        $role->name=$request->name;
+
+        $role->save();
+        return [
+            "payload" => $role,
+            "status" => "200"
+        ];
+    }
+
+    public function delete(Request $request){
+        $role=Role::find($request->id);
+        if(!$role){
+            return [
+                "payload" => "The searched row does not exist !",
+                "status" => "404_4"
+            ];
+        }
+        else {
+            $role->delete();
+            return [
+                "payload" => "Deleted successfully",
+                "status" => "200_4"
+            ];
+        }
+    }
 }
