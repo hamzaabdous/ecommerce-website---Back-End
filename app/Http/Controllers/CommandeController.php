babfd44255afd18668d07a02412e4396da46dd6c
MouadAmzil <mouadmouad2026@gmail.com>
2022-07-25 02:33:01 +0100

    Add router API COMMAND

--- app/Http/Controllers/CommandeController.php
+++ app/Http/Controllers/CommandeController.php
@@ -24,5 +24,48 @@ class CommandeController extends Controller
             "message" => "Book Added."
         ], 201);
     }
-    
+    public function show($id)
+    {
+        $book = Command::find($id);
+        if (!empty($book)) {
+            return response()->json($book);
+        } else {
+            return response()->json([
+                "message" => "Book not found"
+            ], 404);
+        }
+    }
+
+    public function update(Request $request, $id)
+    {
+        if (command::where('id', $id)->exists()) {
+            $book = Command::find($id);
+            $book->name = is_null($request->status_commande) ? $book->status_commande : $request->status_commande;
+            $book->author = is_null($request->quantity) ? $book->quantity : $request->quantity;
+            $book->publish_date = is_null($request->publish_date) ? $book->publish_date : $request->publish_date;
+            $book->save();
+            return response()->json([
+                "message" => "Book Updated."
+            ], 404);
+        } else {
+            return response()->json([
+                "message" => "Book Not Found."
+            ], 404);
+        }
+    }
+    public function destroy($id)
+    {
+        if(Command::where('id', $id)->exists()) {
+            $book = Command::find($id);
+            $book->delete();
+
+            return response()->json([
+              "message" => "records deleted."
+            ], 202);
+        } else {
+            return response()->json([
+              "message" => "book not found."
+            ], 404);
+        }
+    }
 }
--- app/Modules/Commande/routes/api.php
+++ app/Modules/Commande/routes/api.php
@@ -1,4 +1,20 @@
 <?php
 
+use App\Http\Controllers\CommandeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+
+Route::group([
+    'prefix' => 'api/commande'
+
+], function ($router) {
+    Route::get('/', [CommandeController::class, 'index']);
+    Route::get('/{id}', [CommandeController::class, 'get']);
+    Route::post('/create', [CommandeController::class, 'create']);
+    Route::post('/update', [CommandeController::class, 'update']);
+    Route::post('/delete', [CommandeController::class, 'delete']);
+
+});
+
+
