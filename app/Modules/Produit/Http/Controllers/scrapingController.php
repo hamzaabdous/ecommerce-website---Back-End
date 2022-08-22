ed784c81532624184199a30809eddb7e45829549
hamzaabdous <abdoushamza6@gmail.com>
2022-08-22 12:46:09 +0100

    test test scraping

--- app/Modules/Produit/Http/Controllers/scrapingController.php
+++ app/Modules/Produit/Http/Controllers/scrapingController.php
@@ -3,6 +3,7 @@
 namespace App\Modules\Produit\Http\Controllers;
 
 use \stdClass;
+use Illuminate\Support\Facades\Storage;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
@@ -14,11 +15,28 @@ class scrapingController extends Controller
 
     public function scrapingFromEbay()
     {
+       // $listdata= $crawler->filter('.s-card-container .a-section .sg-row .sg-col .sg-col-inner .a-section .sg-row .s-price-instructions-style .a-row .s-color-discount')->each(function ($node) {
 
         $client = new Client(HttpClient::create(['timeout' => 60]));
 
-        $crawler = $client->request('GET', 'https://www.ebay.fr/itm/314078347121?_trkparms=pageci%3Ab9408a57-1a94-11ed-a64a-4213b420711b%7Cparentrq%3A9457cccd1820ad345284605dfffb7f26%7Ciid%3A1');
-        $title= $crawler->filter('.x-item-title .x-item-title__mainTitle .ux-textspans')->each(function ($node) {
+        $crawler = $client->request('GET', 'https://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Daps&field-keywords=samsung&crid=29QTR5Y3BQ21D&sprefix=samsung%2Caps%2C167');
+        $listdata= $crawler->filter('.s-card-container .a-section .sg-row .sg-col .sg-col-inner .s-product-image-container .aok-relative .rush-component .a-link-normal .s-image-fixed-height')->each(function ($node) {
+
+           // $img= $node->filter('.s-image')->count()? $node->filter('.s-item__image-section  .s-item__image .s-item__image-wrapper .s-item__image-img')->attr('src') : null;
+            $img= $node->filter('.s-image')->attr('class');
+            var_dump($img);
+          /*   $produitModule = new stdClass();
+            $produitModule->img =$img;
+
+            $item =[$produitModule];
+
+            return $item[0]; */
+            // $data->push($item);
+             //var_dump($data);
+          });
+
+
+        /* $title= $crawler->filter('.x-item-title .x-item-title__mainTitle .ux-textspans')->each(function ($node) {
           //  dump($node->text());
             return $node->text();
         });
@@ -33,15 +51,13 @@ class scrapingController extends Controller
          $quantity = $crawler->filter('.vi-quantity-wrapper .qtyTxt')->each(function ($node) {
             // dump($node->text());
              return $node->text();
-         });
-        return [
-            "title" => $title[0],
-            "price" => $price[0],
-            "pricedescount" => $pricedescount[0],
-            "quantity" => $quantity[0],
+         }); */
+      /*   return [
+            "listdata" => $listdata,
+
 
             "status" => "200"
-        ];
+        ]; */
     }
 
     public function scrapingFromJumia(Request $request)
@@ -57,6 +73,7 @@ class scrapingController extends Controller
             $title= $node->filter('.info .name')->text();
             $price= $node->filter('.info  .prc')->text();
             $url= $node->attr('href');
+            $img= $node->filter('.img')->attr('src');
 
             $pricedescount= $node->filter('.info  .s-prc-w .old')->count()? $node->filter('.info  .s-prc-w .old')->text() : null;
             $descount= $node->filter('.info  .s-prc-w ._dsct')->count() ? $node->filter('.info  .s-prc-w ._dsct')->text() : null;
@@ -66,6 +83,8 @@ class scrapingController extends Controller
             $produitModule->price =$price;
             $produitModule->pricedescount =$pricedescount;
             $produitModule->descount =$descount;
+            $produitModule->img =$img;
+
             $produitModule->url =$url;
             $item =[$produitModule];
 
